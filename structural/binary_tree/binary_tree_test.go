a3ea469786862b1ec0fd83ddcd2c9eda2038da11
Femi <alayesanmifemi@gmail.com>
2025-09-05 05:00:47 +0100

    added test assertion

--- structural/binary_tree/binary_tree_test.go
+++ structural/binary_tree/binary_tree_test.go
@@ -3,6 +3,7 @@ package binarytree
 import (
 	"fmt"
 	"testing"
+	"reflect"
 )
 
 func TestBinaryTree(t *testing.T) {
@@ -17,6 +18,9 @@ func TestBinaryTree(t *testing.T) {
 	tree.Insert(18)
 
 	fmt.Printf("In-order traversal (sorted): %v\n", tree.InOrder())
+	if !reflect.DeepEqual(tree.InOrder(), []int{2, 5, 8, 10, 12, 15, 18}) {
+		t.Errorf("In-order traversal is not correct")
+	}
 
 }
 
